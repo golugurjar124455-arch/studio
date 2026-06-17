c510337463b9ab89c6aa4d58393312f4f6018315
golugurjar124455-arch <golugurjar124455@gmail.com>
2026-06-17 19:06:49 +0530

    Update Firebase configuration with placeholder API key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 'use client';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAR3Qc7tC-Ebgtk9EIZhuHQvnF6g0or1PA",
+  apiKey: "YOUR_API_KEY",
   authDomain: "studio-7052712596-ce1ac.firebaseapp.com",
   projectId: "studio-7052712596-ce1ac",
-  storageBucket: "studio-7052712596-ce1ac.firebasestorage.app",
+  storageBucket: "studio-7052712596-ce1ac.appspot.com",
   messagingSenderId: "45378465643",
-  appId: "1:45378465643:web:b44f7632193d55c7fde62e"
+  appId: "1:45378465643:web:b44f7632193d55c7fde62e",
 };
